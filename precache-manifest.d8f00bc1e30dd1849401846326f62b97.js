c26556ad737f927d7f424d1e98ce1e46bdadc204
tvthag1611 <bareversewin2000@gmail.com>
2020-03-30 23:09:28 +0700

    Updates

---
+++ precache-manifest.d8f00bc1e30dd1849401846326f62b97.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bbe44e887588ab594b7a7b13b83d8279",
+    "url": "/web/index.html"
+  },
+  {
+    "revision": "997cf2a0c146b8ca2cbd",
+    "url": "/web/static/css/main.9456b24e.chunk.css"
+  },
+  {
+    "revision": "25144c850d306b4976e4",
+    "url": "/web/static/js/2.0ac4e1eb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/web/static/js/2.0ac4e1eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "997cf2a0c146b8ca2cbd",
+    "url": "/web/static/js/main.04519af4.chunk.js"
+  },
+  {
+    "revision": "3ead1b1e6f7da6531f6f",
+    "url": "/web/static/js/runtime-main.2747f626.js"
+  }
+]);
